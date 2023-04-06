912dca8a17458a98d879afc0c94b106c44e00627
awesomerobot <kris.aubuchon@discourse.org>
2023-04-06 17:18:18 -0400

    UX: toggle for fullpage, merge for drawer

--- javascripts/discourse/components/sidebar-mode-toggle.js
+++ javascripts/discourse/components/sidebar-mode-toggle.js
@@ -2,81 +2,81 @@ import Component from "@glimmer/component";
 import { action } from "@ember/object";
 import { inject as service } from "@ember/service";
 import { tracked } from "@glimmer/tracking";
-import { next } from "@ember/runloop";
 
 export default class SidebarModeToggle extends Component {
   @service currentUser;
   @service chatStateManager;
   @service site;
   @service router;
-  @service topicTrackingState;
 
-  @tracked unreadTopicCount;
   @tracked currentMode;
+  @tracked hideToggle = this.chatStateManager.isDrawerPreferred;
 
   constructor() {
     super(...arguments);
+    this.setDefaultMode();
     this.router.on("routeWillChange", (transition) => {
       this.updateModeBasedOnRoute(transition.to);
     });
-
-    this.callbackId = this.topicTrackingState.onStateChange(() => {
-      this.updateUnreadTopicCount();
-    });
-  }
-
-  willDestroy() {
-    this.topicTrackingState.offStateChange(this.callbackId);
-  }
-
-  get currentUserInDnD() {
-    return this.currentUser.isInDoNotDisturb();
-  }
-
-  @action
-  updateUnreadTopicCount() {
-    this.unreadTopicCount = this.topicTrackingState.countUnread();
   }
 
   updateModeBasedOnRoute(routeName) {
     if (routeName.name.startsWith("chat.")) {
-      this.updateSidebarWrapper("chat");
+      this.setChatMode();
     } else {
-      document
-        .querySelector(".sidebar-wrapper")
-        .classList.remove(this.currentMode + "-mode");
+      if (!this.chatStateManager.isDrawerPreferred) {
+        this.setForumMode();
+      } else {
+        this.removeMode();
+      }
     }
   }
 
-  updateSidebarWrapper(newMode) {
+  removeMode() {
     const sidebarWrapper = document.querySelector(".sidebar-wrapper");
-    sidebarWrapper.classList.remove(this.currentMode + "-mode");
-    sidebarWrapper.classList.add(newMode + "-mode");
-    next(() => {
-      this.currentMode = newMode;
-    });
+    sidebarWrapper.classList.remove("chat-mode");
+    sidebarWrapper.classList.remove("forum-mode");
+
+    this.currentMode = null;
+  }
+
+  setChatMode() {
+    const sidebarWrapper = document.querySelector(".sidebar-wrapper");
+    sidebarWrapper.classList.add("chat-mode");
+    sidebarWrapper.classList.remove("forum-mode");
+
+    this.currentMode = "chat";
+  }
+
+  setForumMode() {
+    const sidebarWrapper = document.querySelector(".sidebar-wrapper");
+    sidebarWrapper.classList.add("forum-mode");
+    sidebarWrapper.classList.remove("chat-mode");
+
+    this.currentMode = "forum";
   }
 
   @action
   setDefaultMode() {
-    const isChatRoute = this.router.currentURL.startsWith("/chat/");
-    const defaultMode = isChatRoute ? "chat" : "forum";
-    this.updateSidebarWrapper(defaultMode);
+    if (this.chatStateManager.isDrawerPreferred) {
+      return (this.currentMode = null);
+    }
+
+    if (this.router.currentURL.startsWith("/chat/")) {
+      this.setChatMode();
+    } else {
+      this.setForumMode();
+    }
   }
 
   @action
   toggleMode() {
     if (this.currentMode === "chat") {
-      this.updateSidebarWrapper("forum");
-      if (settings.back_mode) {
-        this.router.transitionTo(
-          this.chatStateManager.lastKnownAppURL.includes("/chat/")
-            ? "/latest"
-            : this.chatStateManager.lastKnownAppURL
-        );
-      }
-    } else {
-      this.updateSidebarWrapper("chat");
+      this.setForumMode();
+      this.router.transitionTo(this.chatStateManager.lastKnownAppURL);
+    } else if (this.currentMode === "forum") {
+      this.setChatMode();
+      this.router.transitionTo(this.chatStateManager.lastKnownChatURL);
     }
   }
 }
